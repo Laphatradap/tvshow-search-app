e97949ff80a72a162fdc48d69cd21ff2915405b7
laphatradap <laphatradap@disroot.org>
2020-05-26 09:19:12 +0200

    Persist redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,23 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./rootReducer";
 import ReduxThunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
   ? window.__REDUX_DEVTOOLS_EXTENSION__()
   : (x) => x;
 
 const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
-const store = createStore(rootReducer, enhancer);
 
-export default store;
+const store = createStore(persistedReducer, enhancer);
+const persistor = persistStore(store);
+
+export { store, persistor };
